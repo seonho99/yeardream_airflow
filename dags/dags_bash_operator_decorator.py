71b915c68070c44af8e1d6a1a93e9398ae3192ab
seonho99 <pandapad10@gmail.com>
2024-06-17 10:29:51 +0900

    1

--- dags/dags_bash_operator_decorator.py
+++ dags/dags_bash_operator_decorator.py
@@ -1,12 +1,24 @@
-import datetime
+vfrom airflow.decorators import dag
+import pendulum
+from airflow.operators.bash import BashOperator
 
-from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+@dag(dag_id='dags_bash_operator_decorator',
+     schedule="0 9 * * 1,5",
+     start_date=pendulum.datetime(2024, 6, 1, tz="Asia/Seoul"),
+     catchup=False,
+     tags=['homework']
+)
+def dags_bash_operator_decorator():
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami"
+    )
 
-with DAG(
-    dag_id='dags_bash_operator_decorator',
-    schedule='0 13 6 * 0#2',
-    start_date=datetime.datetime(2024, 5, 1, tzinfo=datetime.timezone(datetime.timedelta(hours=9))),
-    catchup=False
-):
-    EmptyOperator(tag="homework")
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME"
+    )
+
+    bash_t1 >> bash_t2
+
+dags_bash_operator_decorator()
--- dags/dags_bash_operator_standard.py
+++ dags/dags_bash_operator_standard.py
@@ -1,12 +1,25 @@
-import datetime
-
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+import pendulum
+from airflow.operators.bash import BashOperator
 
-with DAG(
-    dag_id='dags_bash_operator_standard',
-    schedule='0 9 1/6 * *',
-    start_date=datetime.datetime(2024, 6, 1, tzinfo=datetime.timezone(datetime.timedelta(hours=9))),
-    catchup=False
-):
-    EmptyOperator(tag="homework")
+dag = DAG(
+    dag_id="dags_bash_operator_standard",
+    schedule="0 13 * * 5#2",
+    start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
+    catchup=False,
+    tags=['homework']
+)
+
+bash_t1 = BashOperator(
+    task_id="bash_t1",
+    bash_command="echo whoami",
+    dag=dag
+)
+
+bash_t2 = BashOperator(
+    task_id="bash_t2",
+    bash_command="echo $HOSTNAME",
+    dag=dag
+)
+
+bash_t1 >> bash_t2
